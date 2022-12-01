e466cd99b10d6a87199aa7f60a5683243d7038aa
wasek07 <wasek07@ya.ru>
2022-12-01 18:49:49 +0100

    feat: add 01-12

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -201,8 +201,11 @@ function extractEmails(str) {
  *             '└──────────┘\n'
  *
  */
-function getRectangleString(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleString(width, height) {
+  const low = `└${'─'.repeat(width - 2)}┘\n`;
+  const center = `│${' '.repeat(width - 2)}│\n`.repeat(height - 2);
+  const high = `┌${'─'.repeat(width - 2)}┐\n`;
+  return high + center + low;
 }
 
 
