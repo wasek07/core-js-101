84d6369d3653dde9bb6ec95153a526141a4922f1
wasek07 <wasek07@ya.ru>
2022-12-01 19:48:26 +0100

    feat: add 01-14

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -244,11 +244,12 @@ function encodeToRot13(str) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  if (typeof value === 'object') {
+    return value instanceof String;
+  }
+  return typeof value === 'string';
 }
-
-
 /**
  * Returns playid card id.
  *
